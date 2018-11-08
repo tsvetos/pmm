d6d27c0f2b80fc9b07cd92a42206c06dec86c258
tsvetos <seve0955@gmail.com>
2018-11-08 13:37:33 +0100

    AndroidMenu actualizado

--- AndroidMenu/app/src/main/java/com/example/tsvtsv/androidmenu/Ayuda.java
+++ AndroidMenu/app/src/main/java/com/example/tsvtsv/androidmenu/Ayuda.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 
 public class Ayuda extends AppCompatActivity {
 
@@ -11,5 +12,8 @@ public class Ayuda extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ayuda);
+        this.setTitle("Mi ayuda");
     }
+
+
 }
