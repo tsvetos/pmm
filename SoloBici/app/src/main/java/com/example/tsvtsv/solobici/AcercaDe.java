d667770794e348fe4576fb04d4ef17cc4ecdf98a
tsvetos <seve0955@gmail.com>
2018-11-22 13:50:43 +0100

    Ejercicio soloBici montado y funcionando

---
+++ SoloBici/app/src/main/java/com/example/tsvtsv/solobici/AcercaDe.java
@@ -0,0 +1,12 @@
+package com.example.tsvtsv.solobici;
+
+import android.app.Activity;
+import android.os.Bundle;
+public class AcercaDe extends Activity {
+
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        //Hacemos visible la interfaz/layoutque se encuentra en acercade.xml
+        setContentView(R.layout.activity_acercade);
+    }
+}
